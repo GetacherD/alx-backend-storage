53875269f18fbe5c9d88d89cc7fe1a6d0c044e07
GetacherD <getacher.d@gmail.com>
2023-01-18 20:55:55 +0300

    insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,5 +6,6 @@ insert data with pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """ insert all data passed as kwargs"""
-    data = [{key: value} for key, value in kwargs.items()]
-    mongo_collection.insert_many(data)
+    mongo_collection.insert_one(kwargs)
+    res = mongo_collection.find().sort("_id", -1).limit(1)
+    return list(res)[0]["_id"]
