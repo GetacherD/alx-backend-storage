f50a2a3b86d79871dd3f73fd388ebbd2baeb2ac2
GetacherD <getacher.d@gmail.com>
2023-01-19 15:17:28 +0300

    fix input type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 redis basics demo
 """
 import redis
-from typing import Any
+from typing import Union
 from uuid import uuid4
 
 
@@ -14,8 +14,10 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[bytes, float, int, str]) -> str:
         """ store cache object """
         key = str(uuid4())
+        if type(data) not in [bytes, str, int, float]:
+            return ""
         self._redis.set(key, data)
         return key
