0e620570dfd835e14900b6f4f782bc5cc71cb2b7
GetacherD <getacher.d@gmail.com>
2023-01-19 16:53:26 +0300

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 redis basics demo
 """
 from redis import Redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 import json
 
@@ -21,3 +21,22 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float, None]:
+        """ get value associated with the key"""
+        val = self._redis.get(key)
+        if (fn and val):
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """ get str """
+        val = self._redis.get(key)
+        return str(val)
+
+    def get_int(self, key: str) -> int:
+        """ get int """
+        val = self._redis.get(key)
+        if (val):
+            return int(val)
+        return -1
