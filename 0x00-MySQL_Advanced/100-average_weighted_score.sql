a61da1e7a7ad428916aeaf960213080066e92266
GetacherD <getacher.d@gmail.com>
2023-01-25 18:15:23 +0300

    first

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,23 +1,14 @@
 -- compute average weighted score
 DELIMITER ~
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE u_id INT;
-	DECLARE offset INT;
 	DECLARE avgSC FLOAT;
-	SET u_id = (SELECT id FROM users ORDER BY id ASC LIMIT 1);
-	WHILE u_id IS NOT NULL DO
-		
-		SET avgSC = (SELECT SUM(score * weight) / SUM(weight)
-		    FROM users
-		    JOIN corrections ON users.id = corrections.user_id
-		    JOIN projects ON projects.id=corrections.project_id
-		    GROUP BY users.id
-		    HAVING users.id = u_id);
-		UPDATE users SET average_score=avgSc WHERE id=u_id;
-		SET offset = u_id;
-		SET u_id = (SELECT id FROM users
-		    ORDER BY id ASC LIMIT 1 OFFSET offset);
-	END WHILE;
+	SET avgSC = (SELECT SUM(score * weight) / SUM(weight)
+	    FROM users
+	    JOIN corrections ON users.id = corrections.user_id
+	    JOIN projects ON projects.id=corrections.project_id
+	    GROUP BY users.id
+	    HAVING users.id = user_id);
+	UPDATE users SET average_score=avgSC WHERE id=user_id;
 END~
 DELIMITER ;
