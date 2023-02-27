96f09826c7433e168ddfbb4862f4639bd5970c13
Getacher  Demisse <GetacherD@ethiopianairlines.com>
2023-02-27 20:12:56 +0300

    fix annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """
 redis basics demo
 """
-from redis import Redis
+import redis
 from typing import Union, Callable, Optional
 from uuid import uuid4
 import json
@@ -13,10 +13,10 @@ class Cache:
 
     def __init__(self):
         """initializee"""
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, float, int, str]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store cache object  """
         key = str(uuid4())
         self._redis.set(key, data)
